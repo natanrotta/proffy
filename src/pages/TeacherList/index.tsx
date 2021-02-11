700d376888c4766157e6f4605d24342bc99e4b5d
natan_rotta <natanrotta@gmail.com>
2021-02-11 20:07:13 -0300

    Cria formulario

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Input from '../../components/Input';
 
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
@@ -10,26 +12,39 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="subject">Dia da semana</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="subject">Hora</label>
-                        <input type="text" id="subject" />
-                    </div>
+                <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Química', label: 'Química' },
+                        ]}
+                    />
+                    <Select
+                        name="week_day"
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
             <main>
                 <TeacherItem />
-                
+
             </main>
         </div>
     )
